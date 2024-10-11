03595ea9b9f2dc43b3c7043556100a0ca93a70aa
alroney <alroney98@outlook.com>
2024-10-11 13:57:38 -0400

    Added AboutUs.jsx page. Adjusted Navbar contents.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
+import { AboutUs } from './Pages/AboutUs';
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+import { LoginSignup } from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
-      <Navbar/> 
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/biscuits' element={<ShopCategory category="biscuits"/>}/>
+          <Route path='/trainingTreats' element={<ShopCategory category="trainingTreats"/>}/>
+          <Route path='/aboutUs' element={<AboutUs/>}/>
+
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import './Navbar.css'
 import navLogo from '../Assets/img/logo/papbakery_logo_dark.png'
 import cart_icon from '../Assets/img/shopping-cart.png'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
 
@@ -15,12 +16,14 @@ export const Navbar = () => {
         </div>
         
         <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li> {/**The setMenu() will change the menu value to the value in its parameter. The <hr/> tag is added to the li if menu is equal to the current menu item. */}
-            <li onClick={()=>{setMenu("biscuits")}}>About{menu==="biscuits"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> {/**The setMenu() will change the menu value to the value in its parameter. The <hr/> tag is added to the li if menu is equal to the current menu item. */}
+            <li onClick={()=>{setMenu("biscuits")}}><Link to='/biscuits'>Biscuits</Link>{menu==="biscuits"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("trainingTreats")}}><Link to='/trainingTreats'>Training Treats</Link>{menu==="trainingTreats"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("aboutUs")}}><Link to='/aboutUs'>About Us</Link>{menu==="aboutUs"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt="Cart"/>
+            <Link to='/login'><button>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} alt="Cart"/></Link>
             <div className="nav-cart-count">0</div>
         </div>
     </div>
