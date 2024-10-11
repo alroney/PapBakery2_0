d69df4f95368a8915b0b938255352b52d35047f2
alroney <alroney98@outlook.com>
2024-10-11 11:16:59 -0400

    Initialized Navbar features. Added additional fonts to Assets.

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
+import navLogo from '../Assets/img/logo/papbakery_logo_dark.png'
+import cart_icon from '../Assets/img/shopping-cart.png'
 
 export const Navbar = () => {
+
+    const [menu, setMenu] = useState("home"); //Initialize the menu selection
+
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={navLogo} alt="PapBakery Logo"/>
+            <p>PapBakery</p>
+        </div>
+        
+        <ul className="nav-menu">
+            <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("about")}}>About{menu==="about"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt="Cart"/>
+            <div className="nav-cart-count">0</div>
+        </div>
+    </div>
   )
 }
