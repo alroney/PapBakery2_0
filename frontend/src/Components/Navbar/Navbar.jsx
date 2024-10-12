404c2c7b8bb12a5de7cbdf04fd2065a0b7c86038
alroney <alroney98@outlook.com>
2024-10-12 07:56:21 -0400

    Added New components: - Hero - Item - Popular
    Changed favicon.

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -14,17 +14,18 @@ export const Navbar = () => {
             <img src={navLogo} alt="PapBakery Logo"/>
             <p>Paws & Palms Bakery</p>
         </div>
-        
+
         <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> {/**The setMenu() will change the menu value to the value in its parameter. The <hr/> tag is added to the li if menu is equal to the current menu item. */}
-            <li onClick={()=>{setMenu("biscuits")}}><Link to='/biscuits'>Biscuits</Link>{menu==="biscuits"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("trainingTreats")}}><Link to='/trainingTreats'>Training Treats</Link>{menu==="trainingTreats"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("aboutUs")}}><Link to='/aboutUs'>About Us</Link>{menu==="aboutUs"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> {/**The setMenu() will change the menu value to the value in its parameter. The <hr/> tag is added to the li if menu is equal to the current menu item. */}
+            <li onClick={()=>{setMenu("biscuits")}}><Link style={{ textDecoration: 'none' }} to='/biscuits'>Biscuits</Link>{menu==="biscuits"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("trainingTreats")}}><Link style={{ textDecoration: 'none' }} to='/trainingTreats'>Training Treats</Link>{menu==="trainingTreats"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("aboutUs")}}><Link style={{ textDecoration: 'none' }} to='/aboutUs'>About Us</Link>{menu==="aboutUs"?<hr/>:<></>}</li>
         </ul>
+
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt="Cart"/></Link>
-            <div className="nav-cart-count">0</div>
+            <div className="nav-cart-count">0</div> {/*Cart item count display icon*/}
         </div>
     </div>
   )
---
+++ frontend/src/Components/Popular/Popular.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Popular.css'
+
+export const Popular = () => {
+  return (
+    <div className="popular">
+        <h1>Popular Flavors</h1>
+        
+    </div>
+  )
+}
