23d66afe0d531eb9b7b1ae103e50aa6ccd12681f
alroney <alroney98@outlook.com>
2024-10-13 09:26:47 -0400

    Extensive Styling of ProductDisplay page Styled Breadcrums

--- frontend/src/Components/ProductDIsplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDIsplay/ProductDisplay.jsx
@@ -20,7 +20,7 @@ export const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right">
             <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
+            <div className="productdisplay-right-stars">
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
@@ -28,6 +28,25 @@ export const ProductDisplay = (props) => {
                 <img src={star_dull_icon} alt="" />
                 <p>(10)</p>
             </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                Product Description Here
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>Small</div>
+                    <div>Small Long</div>
+                    <div>Large</div>
+                    <div>Large Long</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className="productdisplay-right-category"><span>Category: </span></p>
+            <p className="productdisplay-right-category"><span>Tags: </span></p>
         </div>
     </div>
   )
