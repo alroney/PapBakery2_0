5334fd4e14a59918c2bd9b845e204173e453b765
alroney <alroney98@outlook.com>
2024-10-14 11:28:03 -0400

    Fixed issue in RelateProducts.jsx

--- frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -10,7 +10,7 @@ export const RelatedProducts = () => {
         <hr />
         <div className="relatedproducts-item">
             {data_product.map((item, i) => {
-                return <Item key={i} id={item.id} name={item.name} image={item.image} price={item} /> 
+                return <Item key={i} id={item.id} name={item.name} image={item.image} price={item.price} /> 
             })}
         </div>
     </div>
