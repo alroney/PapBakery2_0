4ffbd8dde1463b1322c2f30752b18714b20902fe
alroney <alroney98@outlook.com>
2024-10-15 08:05:28 -0400

    Added support for 800px width screens: - Dropdown toggle for navbar.

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useRef, useState} from 'react'
 import './Navbar.css'
 import navLogo from '../Assets/img/logo/papbakery_logo_dark.png'
 import cart_icon from '../Assets/img/icon/cart_icon.png'
+import nav_dropdown from '../Assets/img/icon/nav_dropdown.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
 
@@ -9,6 +10,12 @@ export const Navbar = () => {
 
     const [menu, setMenu] = useState("shop"); //Initialize the menu selection
     const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
 
   return (
     <div className='navbar'>
@@ -20,8 +27,8 @@ export const Navbar = () => {
             </div>
             
         </div>
-        
-        <ul className="nav-menu">
+        <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+        <ul ref={menuRef} className="nav-menu">
             <li onClick={()=>{setMenu("shop")}}><Link className="nav-item"  to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li> {/**The setMenu() will change the menu value to the value in its parameter. The <hr/> tag is added to the li if menu is equal to the current menu item. */}
             <li onClick={()=>{setMenu("biscuits")}}><Link className="nav-item" to='/biscuits'>Biscuits</Link>{menu==="biscuits"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("trainingTreats")}}><Link className="nav-item" to='/trainingTreats'>Training Treats</Link>{menu==="trainingTreats"?<hr/>:<></>}</li>
