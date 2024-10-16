9e3c3b41f917f91772fed7969ff0f27c9756e303
alroney <alroney98@outlook.com>
2024-10-16 11:21:11 -0400

    Fixed main.jsx layout compatibility issue. Added basic content and style to navbar component. Added temporary img for nav-profile img.

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Navbar.css'
+import navLogo from '../../assets/img/logo/papbakery_logo_dark.png'
+import navProfile from '../../assets/img/profile/nav-profile.svg'
 
 const Navbar = () => {
   return (
-    <div className="navebar">
-
+    <div className="navbar">
+        <img src={navLogo} alt="Logo" className="nav-logo" />
+        <img src={navProfile} alt="Profile Img" className="nav-profile" />
     </div>
   )
 }
