a9154ff9f0b5298ebe955aa3b06256c376c1b4f4
alroney <alroney98@outlook.com>
2024-10-16 08:16:15 -0400

    Prepared for connection.

--- backend/index.js
+++ backend/index.js
@@ -11,3 +11,8 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+
+app.use(express.json()); //Any request that is received from response will be automatically passed through json.
+app.use(cors()); //This will allow the reactjs project to connect to express app on the specificed port (4000).
+
+
