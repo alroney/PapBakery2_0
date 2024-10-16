1f15310be2b57b5f5721a7c53699515a8135b64c
alroney <alroney98@outlook.com>
2024-10-16 08:25:40 -0400

    Added connection to MongoDB.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,5 @@
-/**
- * This file will contain all the backend code.
- */
-
 const port = 4000;
+
 //const [name of variable] = require("[name of package]");
 const express = require("express");
 const app = express();
@@ -12,7 +9,16 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
+//Database variables
+const username = encodeURIComponent("alrpapb");
+const password = encodeURIComponent("c3ZVpLGPJ4kCXC");
+const cluster = "cluster0";
+const project = "Papbakery";
+let uri = `mongodb+srv://${username}:${password}@${cluster}.ci6fw.mongodb.net/${project}`;
+
 app.use(express.json()); //Any request that is received from response will be automatically passed through json.
 app.use(cors()); //This will allow the reactjs project to connect to express app on the specificed port (4000).
 
+//Database connection with MongoDB
+mongoose.connect(uri);
 
