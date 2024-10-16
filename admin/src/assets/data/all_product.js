7b7ff20cb1c9f88c1222ddc0857b55f54d88c5bf
alroney <alroney98@outlook.com>
2024-10-16 10:57:33 -0400

    Added all assets from the front-end. Initialized the Admin index style.

---
+++ admin/src/assets/data/all_product.js
@@ -0,0 +1,48 @@
+import biscuit_sm from "../img/product/dog/biscuits/biscuit_sm.png";
+
+//This is where all the products will be.
+
+let all_product = [
+    {
+        id: 1,
+        name: "10ct Pumpkin Biscuit",
+        category: "biscuits",
+        image: biscuit_sm,
+        price: 4.00,
+    },
+
+    {
+        id: 2,
+        name: "20ct Pumpkin Biscuit",
+        category: "biscuits",
+        image: biscuit_sm,
+        price: 8.00,
+    },
+
+    {
+        id: 3,
+        name: "30ct Pumpkin Biscuit",
+        category: "biscuits",
+        image: biscuit_sm,
+        price: 12.00,
+    },
+
+    {
+        id: 4,
+        name: "50ct Pumpkin Biscuit",
+        category: "biscuits",
+        image: biscuit_sm,
+        price: 16.00,
+    },
+    
+    // {
+    //     id: ,
+    //     name: ,
+    //     category: ,
+    //     image: ,
+    //     price: ,
+    // },
+
+];
+
+export default all_product;
---
+++ admin/src/assets/data/data.js
@@ -0,0 +1,24 @@
+import bisSmLong from '../img/product/dog/biscuits/biscuit_sm_long.png'
+import bisSm from '../img/product/dog/biscuits/biscuit_sm.png'
+import bisLgLong from '../img/product/dog/biscuits/biscuit_lg_long.png'
+import bisLg from '../img/product/dog/biscuits/biscuit_lg.png'
+
+//The id should be the same as one of the id's in all_product.js
+
+let data_product = [
+    {
+        id:1,
+        name: "Small Long Pumpkin Biscuit",
+        image: bisSmLong,
+        price: 10,
+    },
+
+    {
+        id:2,
+        name: "Small Pumpkin Biscuit",
+        image: bisSm,
+        price: 10,
+    },
+];
+
+export default data_product;
