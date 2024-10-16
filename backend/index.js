c21c453c5601394e6386644727c49749f136b69d
alroney <alroney98@outlook.com>
2024-10-16 09:02:35 -0400

    Created Schema for product creation.

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,37 @@ app.post("/upload", upload.single('product'), (req,res) => {//field name is prod
     })
 })
 
-
+//Schema for creating products
+const Product = mongoose.model("Product", {
+    id:{
+        type: Number,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+    available:{
+        type: Boolean,
+        default: true,
+    }
+})
 
 app.listen(port, (error) => {
     if(!error) {
