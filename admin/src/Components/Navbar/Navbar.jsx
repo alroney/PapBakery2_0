c6ba8f0044469612e6e39fbc37bd2cff8d957f3a
alroney <alroney98@outlook.com>
2024-10-17 15:31:16 -0400

    Optimized images. Removed unused images.

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './Navbar.css'
 import navLogo from '../../assets/img/logo/papbakery_logo_dark.png'
 import navProfile from '../../assets/img/profile/nav-profile.svg'
+import textLogo from '../../assets/img/logo/papbakery_textLogo_dark.png'
+import fullLogo from '../../assets/img/logo/papbakery_titleLogo.png'
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <img src={navLogo} alt="Logo" className="nav-logo" />
         <img src={navProfile} alt="Profile Img" className="nav-profile" />
+        <img src={fullLogo}/>
     </div>
   )
 }
