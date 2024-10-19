26a29aa54f1a65aa3b6f4128bcbd3ca566b86512
alroney <alroney98@outlook.com>
2024-10-19 09:53:36 -0400

    Reconfigured the Popular component to use the API endpoint to fetch the popular products from the database instead of local files.

--- backend/index.js
+++ backend/index.js
@@ -222,7 +222,7 @@ const upload = multer({storage:storage})
 
     //API Endpoint for newest items.
     app.get('/newitems', async (req,res) => {
-        let products = await Product.find({});
+        let products = await Product.find({}); //Get all products.
         let newItems = products.slice(1).slice(-8); //Retrieve the last 8 most recently added products.
         console.log("New Items Fetched.");
         res.send(newItems);
@@ -230,7 +230,7 @@ const upload = multer({storage:storage})
 
     //API Endpoint for popular flavors
     app.get('/popularflavors', async (req,res) => {
-        let products = await Product.find({}); //Get all products
+        let products = await Product.find({}); //Get all products.
         let popular_flavors = products.slice(0,4);
         console.log("Popular Flavors Fetched.");
         res.send(popular_flavors);
--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popular.css'
-import data_product from '../Assets/data/data'
 import Item from '../Item/Item'
 
 export const Popular = () => {
+
+  const [popular_flavors, setPopular_flavors] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/popular') //Call the API endpoint.
+    .then((response) => response.json()) //Then convert the response into json format.
+    .then((data) => setPopular_flavors(data)) //Then place all the data into popular_flavors using its set property.
+  }, []); //[] indicates to be called only one time when component is mounted.
+
   return (
     <div className="popular">
         <h1>Popular Flavors</h1>
         <hr />
         <div className="popular-item">
             {
-              data_product.map((item, i) => {
+              popular_flavors.map((item, i) => {
                 return <Item key={i} id={item.id} name={item.name} image={item.image} price={item.price} /> 
               })
             }
