7c24e79f3c4f0f87812b20365aa3aec927c5bbfa
alroney <alroney98@outlook.com>
2024-10-19 10:13:15 -0400

    Added and attached Middleware to the addtocart API endpoint. Still in test phase for said API.

--- backend/index.js
+++ backend/index.js
@@ -236,11 +236,28 @@ const upload = multer({storage:storage})
         res.send(popular_flavors);
     })
 
-    
+    //Creating Middleware to fetch user.
+    const fetchUser = async (req,res,next) => {
+        const token = req.header('auth-token'); //req.header(keyname);
+        if(!token) {
+            res.status(401).send({errors: "Please authenticate using valid token."}); //Set status code to 401 and send an error object where key is errors.
+        }
+        else {
+            try {
+                const data = jwt.verify(token, 'secret_ecom'); //The token will be decoded use the salt (secret_ecom) which will be saved into the variable data.
+                req.user = data.user;
+                next();
+            }
+            catch (error) {
+                res.status(401).send({errors: "Please authenticate using valid token."});
+            }
+        }
+
+    }
 
     //API Endpoint for adding cartdata
-    app.post('/addtocart', async (req,res) => {
-        console.log(req.body);
+    app.post('/addtocart', fetchUser, async (req,res) => {
+        console.log(req.body, req.user);
     })
 
     //API Endpoint for listening to port.
