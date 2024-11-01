69e633faa79bc7535b4fafa2adeb1107c76cee13
alroney <alroney98@outlook.com>
2024-11-01 13:25:29 -0400

    Removed external package.jsons. Installed Paypal package in client folder.

--- client/src/Components/Checkout/Checkout.jsx
+++ client/src/Components/Checkout/Checkout.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Checkout.css'
+import {
+  PayPalScriptProvider,
+  usePayPalCardFields,
+  PayPalCardFieldsProvider,
+  PayPalButtons,
+  PayPalNameField,
+  PayPalNumberField,
+  PayPalExpiryField,
+  PayPalCVVField,
+} from "@paypal/paypal-js"
 
 export const Checkout = () => {
 
-
+  cons [isPaying, setIsPaying] = useState(false);
+  const initialOptions = {
+    "client-id": 
+  }
 
   return (
     <div className="checkout">
