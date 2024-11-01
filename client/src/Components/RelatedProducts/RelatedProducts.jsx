c93570a94d6e995ace8a2fd9d9fbed1ef94b1a27
alroney <alroney98@outlook.com>
2024-11-01 09:50:34 -0400

    Preperation for RelatedProducts redux.

--- client/src/Components/RelatedProducts/RelatedProducts.jsx
+++ client/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import './RelatedProducts.css'
-import data_product from '../Assets/data/data'
-import Item from '../Item/Item'
+//import Item from '../Item/Item'
 
 export const RelatedProducts = () => {
   return (
     <div className="relatedproducts">
         <h1>Related Products</h1>
         <hr />
-        <div className="relatedproducts-item">
-            {data_product.map((item, i) => {
-                return <Item key={i} id={item.id} name={item.name} image={item.image} price={item.price} /> 
-            })}
-        </div>
+        <p>Related products to go here</p>
     </div>
   )
 }
