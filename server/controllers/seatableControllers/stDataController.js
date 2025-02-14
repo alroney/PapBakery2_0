133582775bcba90a82f831001b0ea00b43230594
alroney <alroney98@outlook.com>
2025-02-14 08:44:06 -0500

    Successful Test.
    Modularized the code for the table caching.
    Removed all global variables relating to table
    information.

--- server/controllers/seatableControllers/stDataController.js
+++ server/controllers/seatableControllers/stDataController.js
@@ -4,8 +4,7 @@ const { getBaseTokenAndUUID } = require('./stTokenController'); //Import the get
 const fs = require('fs');
 const path = require('path');
 
-let cachedBaseInfo = {};
-let cachedTables = [];
+let count = 0;
 
 
 
@@ -24,9 +23,9 @@ const getBaseInfo = async (req, res) => {
         };
 
         const response = await axios(options);
-        cachedBaseInfo = response.data;
+        const data = response.data;
 
-        return cachedBaseInfo;
+        return data;
     }
     catch(error) {
         console.error("(stcTokener)(getBaseInfo) Error fetching base info: ", error);
@@ -36,44 +35,113 @@ const getBaseInfo = async (req, res) => {
 
 
 
-//Test function to get the available tables for the Maps.
-const getCachedTablesData = () => {
-    const ctd = require('../../cache/cachedTables.json');
-    return ctd;
-}
 
 
-
-//Function: Get the names of the available tables in the SeaTable base.
-const getAvailableTables = async (req, res) => {
-    let temp = [];
-    cachedTables.length = 0; //Clear the cached tables to avoid duplicates.
+const getTables = async (req, res) => {
     try {
-        
-        const baseInfo = await getBaseInfo();
-        const baseInfoTables = baseInfo.tables;
-
-        if(cachedTables.length === 0) {
-            console.log("Fetching tables.");
-            baseInfo.tables.forEach(table => {
-                temp.push(table.name);
-            });
+        const filePath = path.join(__dirname, '../../cache/cachedTables.json');
+        if(!fs.existsSync(filePath) && count < 3) {
+            count++;
+            console.log("(getTables) File: ", filePath," not found! Attempting to sync data. Attempt: ", count);
+            await syncSeaTableData();
+            return getTables(req, res);
         }
-
-        cachedTables = temp;
-        await cacheAllTablesData(baseInfoTables); //Cache the data of all tables available.
-        res.status(200).json({ success: true, tables: cachedTables });
+        else {
+            count = 0;
+            const ct = require(filePath).tableList;
+            res.status(200).json({ success: true, message: "Data synced successfully.", tables: ct });
+            return ct;
+        }
     }
     catch(error) {
-        console.error("(stDataController)(getAvailableTables) Error fetching tables: ", error);
+        console.error("(stDataController)(getTables) Error getting tables: ", error);
         res.status(500).json({ success: false, error: error.message });
     }
 }
 
 
+const getTablesData = async () => {
+    try {
+        const filePath = path.join(__dirname, '../../cache/cachedTables.json');
 
-//Function: Assign the data of a table from the baseInfoTables object according to the table name.
-const assignTableData = async (tableName, bIT) => {
+        if(!fs.existsSync(filePath) && count < 3) {
+            count++;
+            console.log("(getTablesData) File: ", filePath," not found! Attempting to sync data. Attempt: ", count);
+            await syncSeaTableData();
+        }
+        else {
+            console.log("File found! ");
+            count = 0;
+            const ctd = require(filePath).tablesData;
+            return ctd;
+        }
+    }
+    catch(error) {
+        console.error("(stDataController)(getTablesData) Error getting tables data: ", error);
+    }
+}
+
+
+
+
+const syncSeaTableData = async () => {
+    try {
+        const baseInfo = await getBaseInfo();
+        const baseInfoTables = baseInfo.tables;
+        const cachedTables = [];
+        const tablesData = [];
+
+        baseInfoTables.forEach(table => {
+            cachedTables.push(table.name);
+        });
+
+        for(const tableName of cachedTables) {
+            try {
+                const tableData = await syncTableData(tableName, baseInfoTables);
+                tablesData.push({
+                    tableName: tableName,
+                    data: tableData,
+                });
+            }
+            catch(error) {
+                console.error(`(stDataController)(syncSeaTableData) Error fetching ${tableName} table data: `, error);
+            }
+        }
+
+        const filePath = path.join(__dirname, '../../cache/cachedTables.json');
+        let storedData = {
+            lastUpdated: new Date(),
+            tableList: cachedTables,
+            tablesData: tablesData,
+        };
+
+        if (!fs.existsSync(filePath)) {
+            console.log("Creating new cache file.");
+            const dirPath = path.dirname(filePath);
+            fs.mkdirSync(dirPath, { recursive: true });
+            fs.writeFileSync(filePath, JSON.stringify(storedData, null, 2));
+            return;
+        }
+
+        const existing = JSON.parse(fs.readFileSync(filePath));
+        existing.tablesData = storedData.tablesData;
+        existing.lastUpdated = storedData.lastUpdated;
+        storedData = existing;
+
+        fs.writeFileSync(filePath, JSON.stringify(storedData, null, 2));
+
+        console.log("All tables cached successfully.");
+        return;
+    }
+    catch(error) {
+        console.error("(stDataController)(syncSeaTableData) Error loading table data: ", error);
+    }
+}
+
+
+
+//Function: Synchronize the data of a table from the baseInfoTables object according to the table name.
+const syncTableData = async (tableName, bIT) => {
     try {
         const table = bIT.find(table => table.name === tableName); //Find the table info by the table name.
         if (!table || !table.columns || !table.rows) {
@@ -102,50 +170,7 @@ const assignTableData = async (tableName, bIT) => {
         return data;
     }
     catch(error) {
-        console.error("(seatableController)(assignTableData) Error assigning table data: ", error);
-    }
-}
-
-
-
-//Function: Retrieve list of available tables and cache the data of each table, calling fetchTableData for each table.
-const cacheAllTablesData = async (baseInfoTables) => {
-    let cachedTablesData = []; //Cached tables data.
-    let ct = cachedTables;
-    const bIT = baseInfoTables;
-    const lastUpdated = new Date(); //Get the current date and time.
-    try {
-        cachedTablesData.length = 0; //Clear cachedTablesData to avoid duplicates.
-        cachedTablesData.push({ lastUpdated: lastUpdated }); //Store the last updated date.
-        //Iterate over cachedTables and fetch data for each table.
-        for(const tableName of ct) {
-            try {
-                const tableData = await assignTableData(tableName, bIT);
-                cachedTablesData.push({
-                    tableName: tableName, //Store the table name.
-                    data: tableData, //Store the fetched data.
-                });
-            }
-            catch(error) {
-                console.error(`(seatableController)(loadTableData) Error fetching ${tableName} table data: `, error);
-            }
-            
-        }
-
-
-        const filePath = path.join(__dirname, '../../cache/cachedTables.json');
-        fs.writeFileSync(filePath, JSON.stringify(cachedTablesData, null, 2)); //Write the cached tables data to a file. (The data to be converted to JSON string, no replacer function used, 2 spaces used for indentation).
-
-        console.log("All tables cached successfully.");
-        //Clear all the temp variables.
-        tempDate = 0;
-        tempCount = 0;
-        tempBT = "";
-        tempUUID = "";
-        return cachedTablesData;
-    }
-    catch(error) {
-        console.error("(seatableController)(loadTableData) Error loading table data: ", error);
+        console.error("(stDataController)(syncTableData) Error assigning table data: ", error);
     }
 }
 
@@ -154,18 +179,18 @@ const cacheAllTablesData = async (baseInfoTables) => {
 //Function: Get the data from a table specified by the table name in the SeaTable base, given by a select component in the frontend.
 const getTableData = async (req, res) => {
     try {
-        let cachedTablesData = getCachedTablesData();
+        const cachedTablesData = await getTablesData(); //Get the cached tables data. Using 'await' to ensure all data is received and does not return a promise.
         const { tableName } = req.params;
-        if(cachedTablesData.length === 0) {
-            await getAvailableTables();
+        if(!cachedTablesData || cachedTablesData.length === 0) {
+            await syncSeaTableData();
         }
-        const tableData = cachedTablesData.find(table => table.tableName === tableName);
+        const tableData = cachedTablesData.find(table => table.tableName === tableName); 
         res.status(200).json(tableData.data);
     }
     catch(error) {
-        console.error("(seatableController)(getTableData) Error getting table data: ", error);
+        console.error("(stDataController)(getTableData) Error getting table data: ", error);
         res.status(500).json({ success: false, error: error.message });
     }
 }
 
-module.exports = { getAvailableTables, getTableData, getCachedTablesData };
+module.exports = { getTables, getTableData, getTablesData };
--- server/routes/seatableRoutes.js
+++ server/routes/seatableRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { updateRows, calculate } = require('../controllers/seatableController');
 const { testSTCMaps, updateProductsTable } = require('../controllers/seatableControllers/stcTestMap');
-const { getAvailableTables, getTableData } = require('../controllers/seatableControllers/stDataController');
+const { getTableData, getTables } = require('../controllers/seatableControllers/stDataController');
 // const checkAuth = require('../middlewares/seatableMiddleware');
 
-router.get('/tables', getAvailableTables);
+router.get('/tables', getTables);
 router.get('/table/:tableName', getTableData);
 router.get('/test', testSTCMaps);
 router.get('/updateProductsTable', updateProductsTable);
